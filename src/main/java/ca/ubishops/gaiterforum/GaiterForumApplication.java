908283cf6bf19f49e1a9a2408b3e7a50077bd2f8
Adam Xu <xuxuanqi13@gmail.com>
2020-05-06 13:26:23 -0400

    initial commit

---
+++ src/main/java/ca/ubishops/gaiterforum/GaiterForumApplication.java
@@ -0,0 +1,13 @@
+package ca.ubishops.gaiterforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaiterForumApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GaiterForumApplication.class, args);
+	}
+}
